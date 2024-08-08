89a781645bd62564d98357fbec74ddccfbc6eb4d
Beatriz <125318073+mbsousa@users.noreply.github.com>
2024-08-08 18:37:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,114 @@ from ulid import ULID
 #instância
 app = FastAPI()
 
+esportes = [
+    {
+        'id': str(ULID()),
+        'nome': 'Vôlei',
+        'tipo': 'Feminino',
+        'coletivo': True
+    },
+    {
+        'id': str(ULID()),
+        'nome': 'Skate', 
+        'tipo': 'Masculino',
+        'coletivo': False
+    },    
+    {
+        'id': str(ULID()),
+        'nome': 'Salto em distância', 
+        'tipo': 'Feminino',
+        'coletivo': False
+    },
+    {
+        'id': str(ULID()),
+        'nome': 'Lançamento de disco', 
+        'tipo': 'Feminino',
+        'coletivo': False
+    },
+    {
+        'id': str(ULID()),
+        'nome': 'Taekwondo', 
+        'tipo': 'Masculino',
+        'coletivo': False
+    },
+    {
+        'id': str(ULID()),
+        'nome': 'Tênis de mesa', 
+        'tipo': 'Masculino',
+        'coletivo': False
+    },
+    {
+        'id': str(ULID()),
+        'nome': 'Escalada Esportiva', 
+        'tipo': 'Feminino',
+        'coletivo': False
+    },
+    {
+        'id': str(ULID()),
+        'nome': 'Judô', 
+        'tipo': 'Feminino',
+        'coletivo': False
+    },
+        {
+        'id': str(ULID()),
+        'nome': 'Corrida', 
+        'tipo': 'Feminino',
+        'coletivo': False
+    },
+]
+
+""" Classe usada pra ser referenciada no método post """
+class Esporte(BaseModel):
+    nome: str
+    tipo: str
+    coletivo: bool
+
+""" Retorna a lista de esportes """
+@app.get("/esportes")
+def esportes_lista():
+    return esportes
+
+""" Isola a lista de esportes com endpoint do id específico """
+@app.get("/esportes/{id}")
+def esporte_detalhes(id:str):
+    for esporte in esportes:
+        if esporte['id'] == id:
+            return esporte
+    return {}
+
+""" Cria uma nova lista de esportes """
+@app.post('/esportes')
+def esporte_criar(esporte: Esporte):
+    esportes.append({
+        'id': str(ULID()),
+        'nome': esporte.nome,
+        'tipo': esporte.tipo,
+        'coletivo': esporte.coletivo,
+    })
+    return {}
+
+""" Deleta uma lista de esportes """
+@app.delete('/esportes')
+def esporte_delete(esporte: Esporte):
+    for elemento in esportes:
+        if elemento['id'] == id:
+            esportes.remove(elemento)
+            return {}
+    return {}
+
+""" Atualiza lista de esportes """
+@app.put('/esportes/{id}')
+def esporte_atualizar(id:str, esporte_atualizado: Esporte):
+    for elemento in esportes:
+        if elemento['id'] == id:
+            elemento['nome'] = esporte_atualizado.nome
+            elemento['tipo'] = esporte_atualizado.tipo
+            elemento['coletivo'] = esporte_atualizado.coletivo
+            return elemento
+    return {}
+
+""" Home da page """
 @app.get("/home")
 def home():
     return "Bem vindos aos Jogos Olímpicos 2024"
