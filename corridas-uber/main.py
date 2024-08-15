d5b0a3f8be08febb16d4bf373d217ca9a2ed9fb5
Beatriz <125318073+mbsousa@users.noreply.github.com>
2024-08-14 21:39:26 -0300

    Add files via upload

--- corridas-uber/main.py
+++ corridas-uber/main.py
@@ -14,6 +14,8 @@ from ulid import ULID
 app = FastAPI()
 
 class Corrida(BaseModel):
+    """ na criação da corrida o id não existe, então será None inicialmente; 
+     depois que o objeto de corrida é criado e armazenado, o ID será gerado automaticamente """
     id: str 
     origem: str
     destino: str
@@ -54,11 +56,15 @@ corridas: list[Corrida] = [
         estado='Finalizada'),
 ]
   
-
-""" Filtra a lista pelo estado """
-""" Dúvida """
 @app.get('/corridas')
 def corrida_lista() -> list[Corrida]:
+    return corridas
+
+""" Filtra a lista pelo estado """
+@app.get('/corridas/{estado}')
+def lista_estado(estado:str) -> list[Corrida]:
+    if estado:
+        return [c for c in corridas if c.estado.lower() == estado.lower()]
     return corridas
 
 """ Detalha a corrida específica pelo ID """
@@ -87,30 +93,30 @@ def corrida_atualizar(id:str, corrida_atualizar: Corrida):
                 c.origem = corrida_atualizar.origem
                 c.destino = corrida_atualizar.destino
                 c.distancia = corrida_atualizar.distancia
-                c.valor = calculo_valor(c.distancia) 
+                c.valor = calculo_valor(corrida_atualizar.distancia) 
                 return c
             raise HTTPException(status_code=400, detail='A corrida não pode ser alterada.')
     raise HTTPException(status_code=404, detail='Corrida não localizada!')
 
 
-@app.post('/corridas/{id}/iniciar')
-def iniciar(id:str):
+@app.post('/corridas/{id}/iniciar' )
+def iniciar(id:str) -> Corrida:
     for c in corridas:
-        if c.estado == 'Requisitada':
-            c.estado = 'Em andamento'
-            return c 
-        raise HTTPException(status_code=400, detail='A corrida só pode iniciar se estiver requisitada')
+        if c.id == id:
+            if c.estado == 'Requisitada':
+                c.estado = 'Em andamento'
+                return c 
+            raise HTTPException(status_code=400, detail='A corrida só pode iniciar se estiver requisitada')
     raise HTTPException(status_code=404, detail='Corrida não localizada!')
 
-
-
 @app.post('/corridas/{id}/finalizar')
-def finalizar(id:str):
+def finalizar(id:str) -> Corrida:
     for c in corridas:
-        if c.estado == 'Em andamento':
-            c.estado = 'Finalizada'
-            return c
-        raise HTTPException(status_code=400, detail='A corrida só pode ser finalizada se estiver em andamento.')
+        if c.id == id:
+            if c.estado == 'Em andamento':
+                c.estado = 'Finalizada'
+                return c
+            raise HTTPException(status_code=400, detail='A corrida só pode ser finalizada se estiver em andamento.')
     raise HTTPException(status_code=404, detail='Corrida não localizada!')
 
 """ Remove a corrida retornando 204 """
